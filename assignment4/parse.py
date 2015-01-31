54b601d11b816dd62b8d835febd599c2673ab68a
janeylee <janey.leeee@gmail.com>
2015-01-30 19:52:33 -0600

    adding plot function

--- assignment4/parse.py
+++ assignment4/parse.py
@@ -1,5 +1,5 @@
 from __future__ import division
-
+import matplotlib.pyplot as plt
 import csv
 import sys
 
@@ -29,3 +29,24 @@ def get_avg_latlng(neighborhood):
 	return "average latitude = " + str(avglat) + " average longitude = " + str(avglongi)
 print get_avg_latlng(HPpermits)
 
+def zip_code_barchart(neighborhood):
+	counts = {}
+	for permit in range(len(neighborhood)):
+		for number in range(21, 100, 7):
+			if neighborhood[permit][number]:
+				if int(neighborhood[permit][number][0:5]) in counts:
+					counts[int(neighborhood[permit][number][0:5])] += 1
+				else:
+					counts[int(neighborhood[permit][number][0:5])] = 1
+	zips = range(len(counts.keys()))
+	plt.figure(figsize=(15,8))
+	plt.bar(zips, counts.values(), align='center')
+	plt.xticks(zips, counts.keys())
+	plt.xlabel("Zip codes")
+	plt.ylabel("Frequency")
+	plt.title("Bar Chart of Contractor Zip Codes for Permits in Hyde Park")
+	plt.savefig('barchart.jpg')
+
+	plt.show()
+
+zip_code_barchart(HPpermits)
