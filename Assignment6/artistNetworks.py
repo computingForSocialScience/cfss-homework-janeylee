b41fde77951575a341b4bfced954d9024debc883
janeylee <janey.leeee@gmail.com>
2015-02-15 17:45:55 -0600

    edits to part 1

--- Assignment6/artistNetworks.py
+++ Assignment6/artistNetworks.py
@@ -13,7 +13,7 @@ def getRelatedArtists(artistID):
 		list_of_artists.append(artist_id)
 	return list_of_artists
 
-# getRelatedArtists("2mAFHYBasVVtMekMUkRO9g")
+# print getRelatedArtists("2mAFHYBasVVtMekMUkRO9g")
 
 def getDepthEdges(artistID, depth):
 	related_artists = getRelatedArtists(artistID) #gives a list of artists
@@ -31,24 +31,28 @@ def getDepthEdges(artistID, depth):
 		while depth > 1: 
 			for each in tuples: # [('A','B'), ('A','C')]
 				more_artists = getRelatedArtists(each[1]) #[D, E]
-				new += [(each[1],x) for x in more_artists]
+				new += [(each[1],x) for x in more_artists if (each[1],x) not in new]
+				# i don't think there would be duplicates
+				# in tuples it will be a,b and in new it will be b,a
+				# becuase i take the 2nd letter as the first in new
 			depth = depth - 1
 		tuples += new
 		return tuples
 
-# print getDepthEdges('2mAFHYBasVVtMekMUkRO9g', )
+
 
 
 def getEdgeList(artistID, depth):
 	data = getDepthEdges(artistID, depth)
 	df = pd.DataFrame(data)
+	df.columns = ["artist1","artist2"]
 	return df
 
-print getEdgeList('2mAFHYBasVVtMekMUkRO9g',2)[0:10]
+# print getEdgeList('2mAFHYBasVVtMekMUkRO9g',2)
 
 def writeEdgeList(artistID,depth,filename):
 	doc = getEdgeList(artistID, depth)
-	doc.to_csv(filename, index=False, header=False)
+	doc.to_csv(filename, index=False)
 
-# writeEdgeList('2mAFHYBasVVtMekMUkRO9g',2,'Edges.csv')
+writeEdgeList('2mAFHYBasVVtMekMUkRO9g',2,'Edges.csv')
 
