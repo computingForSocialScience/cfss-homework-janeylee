1dd00654211d9e9a5b1ae7590d38cd462002fe5b
janeylee <janey.leeee@gmail.com>
2015-03-18 09:06:33 -0500

    finished up to part 2.1

--- final_project/collectData.py
+++ final_project/collectData.py
@@ -24,22 +24,26 @@ def fetchTableInfo(tableID):
     	title = data["table_title"]
     	denom = data["denominator_column_id"]
     	column_list.append((key, value["column_title"], title, tableID, denom))
-    
+
     return column_list
 
-# print fetchTableInfo("B19001")
+print fetchTableInfo("B19001")
 
 def downloadTableData(tableID, state_name):
 	"""Downloads table data by state"""
-	fips_URL = "http://cfss.uchicago.edu/data/FIPS.json"
-	fips_req = requests.get(fips_URL)
-	if not fips_req.ok:
-		print "error in request"
-	fips_dict = fips_req.json()
-	stateid = fips_dict[state_name]
-
+	states = {"Mississippi": 28, "Oklahoma": 40, "Delaware": 10, "Minnesota": 27, "Illinois": 17, "Arkansas": "05", 
+	"New Mexico": 35, "Indiana": 18, "Maryland": 24, "Louisiana": 22, "Idaho": 16, "Wyoming": 56, 
+	"Tennessee": 47, "Arizona": "04", "Iowa": 19, "Michigan": 26, "Kansas": 20, "Utah": 49, 
+	"Virginia": 51, "Oregon": 41, "Connecticut": "09",  "Montana": 30, "California": "06", "Massachusetts": 25, 
+	"West Virginia": 54, "South Carolina": 45, "New Hampshire": 33, "Wisconsin": 55, "Vermont": 50, 
+	"Georgia": 13, "North Dakota": 38, "Pennsylvania": 42, "Florida": 12, "Alaska": "02", "Kentucky": 21, 
+	"Hawaii": 15, "Nebraska": 31, "Missouri": 29, "Ohio": 39, "Alabama": "01", "New York": 36, "South Dakota": 46, 
+	"Colorado": "08", "New Jersey": 34, "Washington": 53, "North Carolina": 37, "District of Columbia": 11, 
+	"Texas": 48, "Nevada": 32, "Maine": 23, "Rhode Island": 44}
+	stateid = states[state_name]
 	state_data_URL = "http://api.censusreporter.org/1.0/data/show/latest?table_ids=%s&geo_ids=140|04000US%s" % (tableID, stateid)
 	state_data_req = requests.get(state_data_URL)
+
 
 	if not state_data_req.ok:
 		print "error in request"
@@ -47,20 +51,17 @@ def downloadTableData(tableID, state_name):
 
 	big = []
 	for geo in state_data['data']:
-		table_info = tuple(state_data['data'][geo][tableID]['estimate'].values())
-		info = (geo, stateid) + table_info
-		big.append(info)
+		mydict= {}
+		mydict["geoID"] = geo
+		mydict["Fips"] = stateid
+		mydict.update(state_data['data'][geo][tableID]['estimate'])
+
+		table_info = (mydict["geoID"], mydict["Fips"])
+
+		for columnID in sorted(state_data['tables'][tableID]['columns'].keys()):
+			table_info+= (mydict[columnID],)	
+		big.append(table_info)
 
 	return big
 
-# print downloadTableData("B19001", "Alabama")
-
-# [u'14000US17031062700', '1', 203.0, 44.0, 160.0, 193.0, 0.0, 37.0, 96.0, 20.0, 0.0, 43.0, 40.0, 31.0, 14.0, 75.0, 46.0, 141.0, 1143.0]
-# should this also get the column headers.
-
-
-
-
-
-
-
+# print downloadTableData("B19001", "Maine")
--- final_project/database.py
+++ final_project/database.py
@@ -4,7 +4,17 @@ import pymysql
 
 list_of_tables = ['B19001', 'B27001']
 
-dbname="acs_survey"
+states = {"Mississippi": 28, "Oklahoma": 40, "Delaware": 10, "Minnesota": 27, "Illinois": 17, "Arkansas": "05", 
+"New Mexico": 35, "Indiana": 18, "Maryland": 24, "Louisiana": 22, "Idaho": 16, "Wyoming": 56, 
+"Tennessee": 47, "Arizona": "04", "Iowa": 19, "Michigan": 26, "Kansas": 20, "Utah": 49, 
+"Virginia": 51, "Oregon": 41, "Connecticut": "09",  "Montana": 30, "California": "06", "Massachusetts": 25, 
+"West Virginia": 54, "South Carolina": 45, "New Hampshire": 33, "Wisconsin": 55, "Vermont": 50, 
+"Georgia": 13, "North Dakota": 38, "Pennsylvania": 42, "Florida": 12, "Alaska": "02", "Kentucky": 21, 
+"Hawaii": 15, "Nebraska": 31, "Missouri": 29, "Ohio": 39, "Alabama": "01", "New York": 36, "South Dakota": 46, 
+"Colorado": "08", "New Jersey": 34, "Washington": 53, "North Carolina": 37, "District of Columbia": 11, 
+"Texas": 48, "Nevada": 32, "Maine": 23, "Rhode Island": 44}
+
+dbname="acs_survey2"
 host="localhost"
 user="root"
 passwd=""
@@ -21,19 +31,79 @@ create_columninfo = """CREATE TABLE IF NOT EXISTS column_info (
        ENGINE=MyISAM DEFAULT CHARSET=utf8"""
 c.execute(create_columninfo)
 
-
-# create_ACSinfo = """CREATE TABLE IF NOT EXISTS ACSdata (
-	# index INTEGER PRIMARY KEY AUTO_INCREMENT, 
-	# geoID VARCHAR (128), 
-	# FIPS VARCHAR (3),
-#          ) ENGINE = MyISAM DEFAULT CHARSET=utf8"""
-#  It will have as many columns as the corresponding ACS table has
-# HOW DO I KNOW HOW TO MAKE THE TABLE THEN???
-# why do we need index on geo id column and fips code column? how do tht?
-# c.execute(create_songs)
-
-insertQuery = '''INSERT INTO column_info (ACScolumnId, columnName, ACStableId, tableName, denom) VALUES (%s, %s, %s, %s, %s)'''
+insertCol = '''INSERT INTO column_info (ACScolumnId, columnName, ACStableId, tableName, denom) VALUES (%s, %s, %s, %s, %s)'''
 
 for table in list_of_tables:
-	c.executemany(insertQuery, fetchTableInfo(table))
+	c.executemany(insertCol, fetchTableInfo(table))
 c.close()
+
+c=db.cursor()
+
+create_income = """CREATE TABLE IF NOT EXISTS income (
+	geoID VARCHAR (128),
+	Fips VARCHAR (128),
+	B19001001 VARCHAR(128),
+	B19001002 VARCHAR(128), 
+	B19001003 VARCHAR(128), 
+	B19001004 VARCHAR(128),
+	B19001005 VARCHAR(128),  
+	B19001006 VARCHAR(128),
+	B19001007 VARCHAR(128),  
+	B19001008 VARCHAR(128), 
+	B19001009 VARCHAR(128), 
+	B19001010 VARCHAR(128), 
+	B19001011 VARCHAR(128), 
+	B19001012 VARCHAR(128), 
+	B19001013 VARCHAR(128), 
+	B19001014 VARCHAR(128), 
+	B19001015 VARCHAR(128), 
+	B19001016 VARCHAR(128), 
+	B19001017 VARCHAR(128)
+         ) ENGINE=MyISAM DEFAULT CHARSET=utf8"""
+
+c.execute(create_income)
+print "created income chart"
+
+
+
+for state in states.keys():
+	print "starting state" + state
+	insertIncome = '''INSERT INTO income (geoID, Fips, B19001001, B19001002, B19001003, B19001004, B19001005, 
+		B19001006, B19001007, B19001008, B19001009, B19001010, B19001011, B19001012, B19001013, 
+		B19001014, B19001015, B19001016, B19001017) VALUES 
+		(%s, %s, %s, %s, %s, %s,  %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)'''
+	c.executemany(insertIncome, downloadTableData("B19001", state))
+	print "finished state" + state
+
+
+create_commute = """CREATE TABLE IF NOT EXISTS commute (
+	geoID VARCHAR (128),
+	Fips VARCHAR (128),
+	B08393001 VARCHAR(128),
+	B08393002 VARCHAR(128), 
+	B08393003 VARCHAR(128), 
+	B08393004 VARCHAR(128),
+	B08393005 VARCHAR(128),  
+	B08393006 VARCHAR(128),
+	B08393007 VARCHAR(128),  
+	B08393008 VARCHAR(128), 
+	B08393009 VARCHAR(128), 
+	B08393010 VARCHAR(128), 
+	B08393011 VARCHAR(128), 
+	B08393012 VARCHAR(128), 
+	B08393013 VARCHAR(128)
+         ) ENGINE=MyISAM DEFAULT CHARSET=utf8"""
+
+c.execute(create_commute)
+print "created COMMUTE chart"
+
+for state in states.keys():
+	print "starting state" + state
+	insertCommute = '''INSERT INTO commute (geoID, Fips, B08393001, B08393002, B08393003, B08393004, 
+		B08393005, B08393006, B08393007, B08393008, B08393009, B08393010, B08393011, 
+		B08393012, B08393013) VALUES (%s, %s, %s, %s, %s, %s,  %s, %s, %s, %s, %s, %s, %s, %s, %s)''' 
+	c.executemany(insertCommute, downloadTableData("B08303", state))
+	print "finished state" + state
+
+c.close()
+
